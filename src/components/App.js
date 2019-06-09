8363b0b42f51ad59bfdf5eb3fcb0391047faa5cb
Joaco <macbook@Joacos-MacBook-Pro.local>
2019-06-09 02:49:06 -0300

    SearchArtist Method and input onChange Method working

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,34 @@ import { Animate } from "react-animate-mount";
 
 class App extends Component
 {
-    
+    state = {artistQuery: ""}
+
+    updateArtistQuery = event =>
+    {
+        this.setState({artistQuery: event.target.value})
+    }
+
+    searchArtist = () =>
+    {
+        console.log('this.state: ', this.state)
+    }
+
+    handleKeyPress = ({key}) =>
+    {
+        key === 'Enter' ? this.searchArtist() : null;
+    }
+
     render()
     {
         return(
             <div>
                 <h2>Music Master</h2>
+                <input 
+                onChange={this.updateArtistQuery}
+                onKeyPress={this.handleKeyPress}
+                placeholder='Search for Artists'
+                />
+                <button onClick={this.searchArtist}>Search</button>
             </div>
         )
     }
