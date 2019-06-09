58dac4933ee6a66772eca19a73d79b90fe33a20a
JoacoEsteban <dev.joaco.esteban@gmail.com>
2019-06-09 02:22:51 -0300

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import { Animate } from "react-animate-mount";
+
+class App extends Component
+{
+    
+    render()
+    {
+        return(
+            <div>
+                <h2>Music Master</h2>
+            </div>
+        )
+    }
+}
+
+
+
+export default App;
