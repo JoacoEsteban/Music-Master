8362febae6600545635123677438b8fc1b90524a
Joaco Esteban <dev.joaco.esteban@gmail.com>
2019-06-20 04:41:47 -0300

    Halfway implemented with hooks. Pause function not working

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,34 +1,47 @@
-import React, {Component} from 'react'
+import React, {Component, useState} from 'react'
 import { Animate } from "react-animate-mount";
 var numeral = require('numeral');
 import playbutton from  '../resources/play-button.png'
 
-var audioIsPlaying = false; //selfexplanatory
-var trackBeingPlayed = undefined; //this one too
+// var audioIsPlaying = false; //selfexplanatory
+// var trackBeingPlayed = undefined; //this one too
 var trackAudios; //defined inside the <Artist> component with its props
+var TBP = undefined;
+var AIP = false;
+console.log('Up here: ',AIP);
+
+const Artist = ({artist, tracks}) =>
+{
+    var [ audioIsPlaying, setAudioPlaying ] = useState(false);
+    var [ trackBeingPlayed, setTrackBeingPlayed ] = useState(undefined);
+
+
+    trackAudios = tracks.map((track)=> new Audio(track.preview_url)); //tracklist
+    
 
 const Image = ({image}) =>
 {
     return(
-
-        <img src={image.url} className='artist-img' />
+        
+        // <img src={image.url} className='artist-img' />
+        <div className='artist-img' style={{backgroundImage: `url('${image.url}')` }}></div>
         )
-}
-
-const Genres = ({genres}) =>
-{
-    return( <nav>
+    }
+    
+    const Genres = ({genres}) =>
+    {
+        return( <nav>
                 {genres.map((genre, i) => <span key={i}> / {genre}</span> )} /
             </nav>
         )
-}
-
-
-const Track = ({track, index}) =>
-{
-    // var trackAudio = new Audio(track.preview_url);
-    return(
-    <span className='track transition'>
+    }
+    
+    
+    const Track = ({track, index}) =>
+    {
+        // var trackAudio = new Audio(track.preview_url);
+        return(
+            <span className='track transition'>
         <div className='track-img' style={{backgroundImage: `url('${track.album.images[0].url}')` }}>
             <img
             className='skere' 
@@ -58,70 +71,80 @@ const Tracks = ({tracks}) =>
     )
 }
 
-const Artist = ({artist, tracks}) =>
+
+
+const setTBP = (i) =>
 {
-    trackAudios = tracks.map((track)=> new Audio(track.preview_url)); //tracklist
-    return(
-        <div className='artist no-size'>
-               <Image image={artist.images[0]} />
-               <h2>{artist.name}</h2>
-               <code>{numeral(artist.followers.total).format('0,0')} followers</code>
-
-                {/* <Followers total={artist.followers.total} /> */}
-                <Genres genres={artist.genres}/>
-                <Tracks tracks={tracks}/>
-
-                <div className='controls'>
-                    <button
-                        onClick={()=>{handleMedia(trackBeingPlayed)}}
-                    >Play</button>
-                    <button>Pause</button>
-                </div>
-            </div>
-        )
+    TBP = i;
+    setTrackBeingPlayed(i);
 }
-
+const setAIP = (mode) =>
+{
+    AIP = mode;
+    setAudioPlaying(mode);
+}
 
 
 
 const pause = () =>
 {
-    trackAudios[trackBeingPlayed].pause();
-    audioIsPlaying = false;
+    //TODO Fix this shit
+    trackAudios[TBP].pause();
+    console.log(TBP);
+    setAIP(false);
 }
 
-const play = (i) =>
+const play = (index) =>
 {
-    i = i === undefined ? trackBeingPlayed : i;
-    trackAudios[i].play();
-    audioIsPlaying = true;
+    index = index === undefined ? TBP : index;
+    trackAudios[index].play();
+    AIP = true;
+    setAIP(true);
 }
-
 
 const handleMedia = (i) =>
-
 {
-    console.log(i);
     if( i !== undefined )
     {
-        if(audioIsPlaying)
+        if(AIP)
         {
+            console.log('pause', AIP)
             pause();
-        }else if(!audioIsPlaying)
+        }else if(!AIP)
         {
+            console.log('play', AIP)
             play(i);
         }
         
         
-        if(i !== trackBeingPlayed)
+        if(i !== TBP)
         {
-            trackBeingPlayed = i;
-            play();
+            // console.log(TBP);
+            setTBP(i);
+            // play();
         }
         
     }
 }
 
 
+return(
+    <div className='artist no-size'>
+        <Image image={artist.images[0]} />
+        <h2>{artist.name}</h2>
+        <code>{numeral(artist.followers.total).format('0,0')} followers</code>
 
+        {/* <Followers total={artist.followers.total} /> */}
+        <Genres genres={artist.genres}/>
+        <Tracks tracks={tracks}/>
+
+        <div className='controls'>
+            <button
+                onClick={()=>{handleMedia(TBP)}}
+            >{audioIsPlaying ? 'Pause' : 'Play'}</button>
+        </div>
+    </div>
+)
+
+}
 export default Artist;
